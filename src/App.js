0311d1a65059526d1cf8b216d7599e01adae4d35
Anastasia Haller <anadana87@gmail.com>
2023-09-20 14:04:25 +0200

    implement routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Route path='/home'>
+        <Home />
+      </Route>
+      <Route path='/about'>
+        <About />
+      </Route>
+    </div>
+  );
 }
 
 export default App;
