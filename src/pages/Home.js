627e7a1981d1e180138f1d20b3f398e25134a0b1
Anastasia Haller <anadana87@gmail.com>
2023-09-21 14:20:20 +0200

    implement nested routes and user redirection

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
+import { Route } from "react-router-dom/cjs/react-router-dom.min";
+
 const Home = () => {
-    return <h1>Home Page</h1>
+    return (
+        <section>
+            <h1>Home Page</h1>
+            <Route path='/home/new-user'>
+                <h2>Welcome!</h2>
+            </Route>
+        </section>
+    );
 };
 
 export default Home;
