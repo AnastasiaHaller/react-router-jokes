ee2818f796e4a0bf98701b50889eb0ded343edaa
Anastasia Haller <anadana87@gmail.com>
2023-10-20 09:15:55 +0200

    implement JokeDetails

--- src/pages/JokeDetails.js
+++ src/pages/JokeDetails.js
@@ -1,11 +1,31 @@
 import { useParams, Route } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedJoke from "../components/jokes/HighlightedJoke";
+
+const DUMMY_JOKES = [
+    {
+        id: 'j1',
+        topic: 'Programming',
+        text: `How many programmers does it take to change a light bulb? None - It's a hardware problem.`,
+    },
+    {
+        id: 'j2',
+        topic: 'General',
+        text: 'How many bones are in the human hand? A handful of them.',
+    },
+];
 
 const JokeDetails = () => {
     const params = useParams();
+    const joke = DUMMY_JOKES.find((joke) => joke.id === params.jokeId);
+
+    if (!joke) {
+        return <h1>No Jokes</h1>
+    }
+
     return(
         <>
-            <h1>Joke Details Page {params.jokeId}</h1>
+            <HighlightedJoke text={joke.text} topic={joke.topic}/>
             <Route path='/jokes/:jokeId/comments'>
                 <Comments />
             </Route>
