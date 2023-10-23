4f79b26b49f2c1ed71d928a722a84c136d235e06
Anastasia Haller <anadana87@gmail.com>
2023-10-23 09:52:15 +0200

    implement navigation on btn click
    used useHistory for AddJoke

--- src/pages/AddJoke.js
+++ src/pages/AddJoke.js
@@ -1,8 +1,13 @@
 import JokeForm from "../components/jokes/JokeForm";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const AddJoke = () => {
+    const history = useHistory();
+
     const addJokeHandler = (jokeData) => {
         console.log(jokeData);
+
+        history.push('/jokes');
     };
 
     return <JokeForm onAddJoke={addJokeHandler} />;
