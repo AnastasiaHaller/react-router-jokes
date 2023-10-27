2abbf50da120ef34d2e2a1bfa468d53aff2cb4db
Anastasia Haller <anadana87@gmail.com>
2023-10-27 11:32:57 +0200

    Http Requests

--- src/pages/AddJoke.js
+++ src/pages/AddJoke.js
@@ -1,16 +1,24 @@
 import JokeForm from "../components/jokes/JokeForm";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import useHttp from "../hooks/use-http";
+import { addJoke } from "../utils/firebase-api";
+import { useEffect } from "react";
 
 const AddJoke = () => {
     const history = useHistory();
+    const {sendHttpRequest, status} = useHttp(addJoke);
+
+    useEffect(() => {
+        if (status === 'completed') {
+            history.push('/jokes');
+        }
+    }, [status, history]);
 
     const addJokeHandler = (jokeData) => {
-        console.log(jokeData);
-
-        history.push('/jokes');
+        sendHttpRequest(jokeData);
     };
 
-    return <JokeForm onAddJoke={addJokeHandler} />;
+    return <JokeForm isLoading={status === 'pending'} onAddJoke={addJokeHandler} />;
 };
 
 export default AddJoke;
--- src/pages/JokeDetails.js
+++ src/pages/JokeDetails.js
@@ -1,33 +1,57 @@
 import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedJoke from "../components/jokes/HighlightedJoke";
+import useHttp from "../hooks/use-http";
+import { getJoke } from "../utils/firebase-api";
+import { useEffect } from "react";
+import Loader from "../components/UI/Loader";
 
-const DUMMY_JOKES = [
-    {
-        id: 'j1',
-        topic: 'Programming',
-        text: `How many programmers does it take to change a light bulb? None - It's a hardware problem.`,
-    },
-    {
-        id: 'j2',
-        topic: 'General',
-        text: 'How many bones are in the human hand? A handful of them.',
-    },
-];
+// const DUMMY_JOKES = [
+//     {
+//         id: 'j1',
+//         topic: 'Programming',
+//         text: `How many programmers does it take to change a light bulb? None - It's a hardware problem.`,
+//     },
+//     {
+//         id: 'j2',
+//         topic: 'General',
+//         text: 'How many bones are in the human hand? A handful of them.',
+//     },
+// ];
 
 const JokeDetails = () => {
 
     const routeMatch = useRouteMatch();
     const params = useParams();
-    const joke = DUMMY_JOKES.find((joke) => joke.id === params.jokeId);
+    const {jokeId} = params;
 
-    if (!joke) {
-        return <h1 className='centered'>No Jokes</h1>
+    const {sendHttpRequest, status, data: loadedJoke, error} = useHttp(getJoke, true);
+
+    // const joke = DUMMY_JOKES.find((joke) => joke.id === params.jokeId);
+
+    useEffect(() => {
+        sendHttpRequest(jokeId);
+    }, [sendHttpRequest, jokeId]);
+
+    if (status === 'pending') {
+        return (
+            <div className="centered">
+                <Loader />
+            </div>
+        );
+    }
+
+    if (error) {
+        return <p className="centered focused">{error}</p>
+    }
+
+    if (!loadedJoke.text) {
+        return <h1 className='centered'>Joke is not found</h1>
     }
 
     return(
         <>
-            <HighlightedJoke text={joke.text} topic={joke.topic}/>
+            <HighlightedJoke text={loadedJoke.text} topic={loadedJoke.topic}/>
             <Route path={`${routeMatch.path}`} exact>
                 <div className="centered">
                     <Link className='btn--empty' to={`${routeMatch.url}/comments`}>Show Comments</Link>
